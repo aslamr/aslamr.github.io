a50bb23d11ff502a76ce82b66227c4c3412e7111
Aslam Raffee <aslamr@gmail.com>
2020-06-27 21:24:17 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,6 @@
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
 fetch("http://api.alquran.cloud/v1/ayah/262/en.asad")
 	.then((r) => r.json())
 	.then((quran) => {
