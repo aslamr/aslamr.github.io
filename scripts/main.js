0aa45b37a5f7336d84add9287c47e1bf2b484f4c
Aslam Raffee <aslamr@gmail.com>
2020-06-28 10:11:17 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,2 @@
 const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello ZWorldsk!';
-
-
-fetch('https://reqres.in/api/users')
-    .then(res => res.json())
-    .then(res => {
-        res.data.map(user => {
-            console.log(`${user.id}: ${user.first_name} ${user.last_name}`);
-        });
-    });
-
-fetch("http://api.alquran.cloud/v1/ayah/262/en.asad")
-	.then((r) => r.json())
-	.then((quran) => {
-	console.log(quran);
-	handleQuran(quran)
-});
-
-const handleQuran = (quranDetails) => {
-document.querySelector("p").innertext = quranDetails.text;
-};
+myHeading.textContent = 'Hello XWorldsk!';
