502635f151aaac65b1e67a798e90f3e13921c422
Aslam Raffee <aslamr@gmail.com>
2020-06-28 09:55:15 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,11 @@
 const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello TWorldsk!';
+myHeading.textContent = 'Hello LWorldsk!';
 
-
-(async () => {
 let response = await fetch('https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits');
 
 let text = await response.text(); // read response body as text
 
-alert(text);
+alert(text.slice(0, 80) + '...');
 
 
 
