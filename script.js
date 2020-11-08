3e29dba5ae294c7a8aa8193fae6eeeb32643ac41
Aslam Raffee <aslamr@gmail.com>
2020-11-08 17:30:36 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("NETWORK RESPONSE ERROR");
+    }
+  })
+  .then(data => displayCocktail(data))
+  .catch((error) => console.error("FETCH ERROR:", error));
+
+function displayCocktail(data) {
+  const cocktail = data.drinks[0];
+  const cocktailDiv = document.getElementById("cocktail");
+}   
